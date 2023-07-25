b44891a3e38336b6083e9aa3b4733466088cc373
Duncan McPherson <duncan.mcpherson.jobs@gmail.com>
2023-07-25 17:47:30 -0600

    some slight optimizations for repeated use in the same session and for handling prime/i > 0.5 (if is still prime at this point all other primes will have a remainder)

--- src/app/nth-prime/services/nth-prime.service.ts
+++ src/app/nth-prime/services/nth-prime.service.ts
@@ -4,15 +4,21 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class NthPrimeService {
+  private generatedPrimes: number[] = []
   public getNthPrime(n: number): number {
-    const primes: number[] = [
+    const primes: number[] = this.generatedPrimes.length ? this.generatedPrimes : [
       2
     ];
-    let i = 3;
+    let i = primes.length == 1 ? 3 : primes[primes.length - 1];
+    if (primes.includes(i)) {
+      i += 2;
+    }
     while (primes.length < n) {
       let isPrime = true;
-      for (let index = 0; index < primes.length && isPrime; index++) {
+      let isPastHalf = false;
+      for (let index = 1; index < primes.length && isPrime && !isPastHalf; index++) {
         let prime = primes[index];
+        isPastHalf = prime / i > 0.5;
         let modulus = i % prime;
         isPrime = modulus != 0;
       }
@@ -21,6 +27,7 @@ export class NthPrimeService {
       }
       i += 2;
     }
+    this.generatedPrimes = primes;
     return primes[n - 1];
   }
 }
